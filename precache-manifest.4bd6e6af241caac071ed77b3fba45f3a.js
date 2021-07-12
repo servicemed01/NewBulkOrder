ea13e9176871eead3bba990a000034cc06bd30be
Shyam <shyam.webwork@gmail.com>
2021-07-12 13:59:18 +0530

    shyam

---
+++ precache-manifest.4bd6e6af241caac071ed77b3fba45f3a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9a4365b8a1cea2ce4fe",
+    "url": "/static/js/main.c99e487a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "add492e097ccdc2ac15f",
+    "url": "/static/css/2.eb2d2a90.chunk.css"
+  },
+  {
+    "revision": "add492e097ccdc2ac15f",
+    "url": "/static/js/2.0345b314.chunk.js"
+  },
+  {
+    "revision": "38e78a7c723b521ed00d89bb676ed056",
+    "url": "/static/media/rec.38e78a7c.png"
+  },
+  {
+    "revision": "9915fef980fa539085da55b84dfde760",
+    "url": "/static/media/remixicon.9915fef9.woff2"
+  },
+  {
+    "revision": "881fbc46361e0c0e5f003c159b2f3005",
+    "url": "/static/media/remixicon.881fbc46.woff"
+  },
+  {
+    "revision": "31d28485e1cf7369272270fd730327c0",
+    "url": "/static/media/remixicon.31d28485.eot"
+  },
+  {
+    "revision": "888e61f04316f10bddfff7bee10c6dd0",
+    "url": "/static/media/remixicon.888e61f0.ttf"
+  },
+  {
+    "revision": "95138f36e015ad912c37db92164f5844",
+    "url": "/static/media/remixicon.95138f36.svg"
+  },
+  {
+    "revision": "7201b0a70f363bcbe5fb781579c1c051",
+    "url": "/static/media/user-whit.7201b0a7.png"
+  },
+  {
+    "revision": "7de5e5f7d3fbd734c8d9328cc6806365",
+    "url": "/static/media/user.7de5e5f7.svg"
+  },
+  {
+    "revision": "ad07d198153bc5b8e10b6eaed303f3f0",
+    "url": "/index.html"
+  }
+];
