c02981f5eb01297b5020e422f39910c950716298
Shyam <shyam.webwork@gmail.com>
2021-08-07 19:32:37 +0530

    latest build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b8f19da98a4d96ceb18a8cf336af327.js"
+  "/precache-manifest.a38697ef70645d99142d807219b3d10d.js"
 );
 
 workbox.clientsClaim();
